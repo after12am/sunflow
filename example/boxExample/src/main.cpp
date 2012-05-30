1e5838e291570f946f815d69c3635204ea45eb8c
satoshi okami <me.after.12am@gmail.com>
2012-05-30 23:20:17 +0900

    add boxExample

---
+++ example/boxExample/src/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  emptyExample
+//
+//  Created by Okami Satoshi on 12/04/27.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream.h>
+#include <GLUT/GLUT.h>
+#include "sunflow.h"
+#include "testApp.h"
+
+
+int main (int argc, char * argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(10, 10);
+	//glutInitWindowSize(1024, 768);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("emptyExample");
+	
+	sf::RunApp(new testApp());
+	return 0;
+}
---
+++ example/boxExample/src/testApp.cpp
@@ -0,0 +1,91 @@
+
+#include "testApp.h"
+#include <GLUT/GLUT.h>
+#include <iostream.h>
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+	// FILTER_MITCHELL is nice filter
+	sf::setFilter(sf::FILTER_MITCHELL);
+	
+	// apply anti-aliasing
+	sf::smooth();
+	
+	int n = 31;
+	
+	sf::translate(-12, 0, -12);
+	
+	for (int i = -n; i <= n; i++) {
+		
+		for (int j = -n; j <= n; j++) {
+			
+			switch (abs((i + j) % 4)) {
+				case 0:
+					sf::setColor(.8f, .8f, 0);
+					break;
+				case 1:
+					sf::setColor(.8f, 0, .8f);
+					break;
+				case 2:
+					sf::setColor(0, .8f, .8f);
+					break;
+				case 3:
+					sf::setColor(.8f, .8f, .8f);
+					break;
+			}
+			
+			sf::pushMatrix();
+			sf::translate(i, .5f, j);
+			sf::scale(.7f, .7f, .7f);
+			sf::box(1);
+			sf::popMatrix();
+		}
+	}
+	
+	sf::setColor(0, .8f, .8f);
+	sf::floor();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+	
+	if (key == 'r') {
+		sf::render();
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+	
+	float dist = h  * 0.5f / tan(ofDegToRad(camera.fovy * 0.5f));
+	float aspect = (float)w / h;
+	float z = dist / camera.scale * 2;
+	
+	sf::setupScreenPerspective(sf::vec3f(z, 28, z), sf::vec3f(0, 0, 0), sf::vec3f(0, 1, 0), 30, aspect, camera.near, camera.far);
+}
